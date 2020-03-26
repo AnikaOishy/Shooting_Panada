11e38c1dfb64d3acbec77b818ebcbba7deff252f
Oracle <oishy.anika@yahoo.com>
2020-03-26 21:07:07 +0600

    first commit

---
+++ Source.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+ int main()
+{  
+   float a=1.2,b=8.5,c=5.6,d=45.63,e=25.3,f,g,h;
+   //sumation
+   f=a+b+c+d+e;
+
+   printf("sumation of f is:f\n",f);
+
+   g=f/5;
+   printf("average=f\n",g);
+   //product
+   h=a*b*c*d*e;
+   printf("product of h is:h\n",h);
+  
+     return 0;
+ 
+}
